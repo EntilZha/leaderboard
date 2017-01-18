f6a3c50a1a0dad92a8dcf29ace34e4f8e20468db
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-01-17 22:27:38 -0700

    Add user creation form

--- codata/urls.py
+++ codata/urls.py
@@ -21,5 +21,6 @@ from leaderboard import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('authtools.urls'))
+    url(r'^accounts/', include('authtools.urls')),
+    url(r'^users/', include('leaderboard.urls'))
 ]
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,5 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from authtools.forms import UserCreationForm
 
 
 def index(request):
     return render(request, 'leaderboard/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # do stuff
+            return HttpResponseRedirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'leaderboard/register.html', {'form': form})
