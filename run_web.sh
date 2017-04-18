565f67a2a8e54ac53570537e52fb237aa39ddeb9
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-04-18 00:02:58 -0600

    Change production server to use gunicorn

--- run_web.sh
+++ run_web.sh
@@ -7,4 +7,5 @@ sleep 5
 python manage.py makemigrations
 python manage.py makemigrations leaderboard
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
+gunicorn codata.wsgi
